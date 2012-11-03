16851eac58427627af1189e73eef6d3abd5b3273
vishwanth <vishwa.hyd@gmail.com>
2012-11-03 23:49:28 +0530

    Finally success. S2 controlled LEDs!!

--- main.c
+++ main.c
@@ -1,32 +1,44 @@
 #include<msp430g2553.h>
 
-void mydelay(int delay_mul)
-{
-	unsigned int i = 0, j = 0, k = 0;
-	for(j = 0; j < delay_mul; j++)
-		for(i = 0; i < 0xffff; i++)
-			for(k = 0; k < 0xffff; k++);
-}
-
-void main()
+int main(void)
 {
 	// Configure the clock for 1 MHz.
 	BCSCTL1 = CALBC1_1MHZ;
 	DCOCTL = CALDCO_1MHZ;
 	
 	P1DIR |= BIT0 + BIT6;	// Output LEDs
-	P1DIR &= ~BIT3;			// Switch input
+	P1DIR &= ~BIT3;			// use switch as input
+	/* Since we dont have a pull up resistor, but only a pull down, we can
+	 short the P1.3 and P1.4, making P1.4 high. This will connect upper terminal
+	 of switch to +5 and lower to gnd through a pull down resistor.
+	
+		+5
+		___  P1.4
+		 |
+		 |__ P1.3
+	  S2\
+		 |
+		 /
+		 \
+		 / Pull down resistor
+		 \
+		 |
+		_|_
+		 _
+		 .
+	*/	 
+		 
+	
+	P1DIR |= BIT4;			// Create upper voltage terminal
+	P1OUT |= BIT4;
+	P1REN = BIT3;			// Enable pull down.
 	while(1)
 	{
-		if(P1IN&BIT3)
-		{
-			P1OUT = BIT0;
-			__delay_cycles(1000);
-		}
+		if( P1IN & BIT3 )
+			P1OUT = BIT0 + BIT4;
 		else
-		{
-			P1OUT = BIT6;		
-			__delay_cycles(1000);
-		}
+			P1OUT = BIT6 + BIT4;
 	}
+	
+	return 0;
 }
