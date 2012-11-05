ea0155ec520f03e15939b533906cbfb12cfcdce7
vishwanth <vishwa.hyd@gmail.com>
2012-11-05 18:43:50 +0530

    ADC and PWM successful.

--- main.c
+++ main.c
@@ -24,13 +24,30 @@ unsigned int get_sample(void)
 {
 	// Start conversion
 	ADC10CTL0 |= ADC10SC + ENC;
-	while(ADC10CTL1 & ADC10BUSY);
-	return ADC10MEM;
+	while(ADC10CTL1 & ADC10BUSY);	// Halt till ADC conversion is busy.
+	return ADC10MEM;				// Return the ADC value
+}
+
+void pwm_init(void)
+{
+	TACTL = TASSEL_2 + ID_0 + MC_1;		// SMCLK, no divide, up mode.
+	TACCR0 = 0x00ff;					// Counter top value
+	TACCR1 = 0x003f;					// Period of count. 0 initially
+	TACCTL1 = OUTMOD_7;					// TACCR1 set/reset
+	P1DIR |= BIT2;						// P1.2 is output
+	P1SEL |= BIT2;
 }
 
 int main(void)
 {
 	CPUinit();
 	ADCinit();
+	pwm_init();
+	int sample = 0;
+	while(1)
+	{
+		sample = get_sample();
+		TACCR1 = sample;
+	}
 	return 0;
 }
